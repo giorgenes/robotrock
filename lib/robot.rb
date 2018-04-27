0d92f97830bed2a1737653a779e676b3d3e04bf0
Giorgenes <giorgenes@gmail.com>
2018-04-28 05:56:10 +1000

    basic robot mechanics

---
+++ lib/robot.rb
@@ -0,0 +1,78 @@
+require 'matrix'
+
+class Robot
+  # face directions represented as (x,y) vectors
+  FACE_TO_VECTOR_MAP = {
+      'NORTH' => [0, 1].freeze,
+      'SOUTH' => [0, -1].freeze,
+      'WEST'  => [-1, 0].freeze,
+      'EAST'  => [1, 0].freeze,
+  }.freeze
+
+  VECTOR_TO_FACE_MAP = FACE_TO_VECTOR_MAP.invert.freeze
+
+  # 90 and 270 degree rotation matrices
+  # rotation is counter clockwise
+  ROTATION_MATRIX_MAP = {
+    'LEFT'  => Matrix.columns([[0, 1], [-1, 0]]).freeze,
+    'RIGHT' => Matrix.columns([[0, -1], [1, 0]]).freeze
+  }.freeze
+
+  def initialize
+    set_face('NORTH')
+  end
+
+  def set_tabletop_dimensions(x, y)
+    @max_x = x
+    @max_y = y
+  end
+
+  def set_face(face)
+    @face = FACE_TO_VECTOR_MAP[face]
+  end
+
+  def place(position, face)
+    if inside_limits?(position)
+      set_face(face)
+      @position = Matrix.column_vector(position)
+      return true
+    else
+      return false
+    end
+  end
+
+  # computes next position. doest care about limits
+  def next_position
+    (@position + face_vector).column(0).to_a
+  end
+
+  def face
+    VECTOR_TO_FACE_MAP[@face]
+  end
+
+  # rotate the `face` vector either 90 degrees or 270 depending on direction
+  def turn(direction)
+    @face = (ROTATION_MATRIX_MAP[direction] * face_vector).column(0).to_a
+  end
+
+  def move
+    n = next_position
+    if inside_limits?(n)
+      @position = Matrix.column_vector(n)
+    end
+  end
+
+  def position
+    @position.column(0).to_a
+  end
+
+  private
+
+  def face_vector
+    Matrix.column_vector(@face)
+  end
+
+  def inside_limits?(v)
+    v[0] < @max_x && v[0] >= 0 && v[1] < @max_y && v[1] >= 0
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+$: << File.expand_path('../lib', __FILE__)
+
+require 'pry'
+require 'robot'
+
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
